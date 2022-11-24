4c77935948a6cfce1c8f4d4701f080dddc52a073
Alexander Levkin <49125444+Comandosss@users.noreply.github.com>
2022-11-24 17:10:33 +0300

    Delete 'commented out' code

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
-#журнал логов
+#Журнал логов
 import logging
-#Компонент отвечающий за коммуникацию с сервером Телеги(Updater),
-#за обработку команд(CommandHandler)
+#Компонент отвечающий за коммуникацию с сервером Телеги(Updater), за обработку команд(CommandHandler)
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 #Импорт settings.py для сокрытия токенов, пароль и личных данных
 import settings
@@ -12,16 +11,11 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s',
     datefmt='%H:%M:%S')
 
-#Настройка прокси (не работает прокси-сервер, запустил без него)
-#PROXY = {'proxy_url': settings.PROXY_URL,
-#    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
-
 #Функция вызова приветствия
 #update - информация, пришедшая из Телеги(команда 'start', информация о пользователе, которую вызвал эту команду)
 #context - когда мы хотим отправить другому пользователю команду
 def greet_user(update, context):
     print('Вызван /start')
-    #print(update.message.reply_text('Привет, пользователь!'))
     update.message.reply_text('Привет, пользователь!')
 
 #Функция ответа пользователю на входящие сообщение
@@ -33,7 +27,7 @@ def talk_to_me(update, context):
 
 def main():
     #Создаем бота и передаем ему токен для авторизации на серверах Телеги
-    mybot = Updater(settings.API_KEY, use_context=True) #request_kwargs=PROXY)
+    mybot = Updater(settings.API_KEY, use_context=True)
     #Используем диспетчер для того, чтобы при наступлении события вызывалась наша функция main(), start - команда, greet_user - вызов функции
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
@@ -49,5 +43,3 @@ def main():
 #Запуск бота(исполнение только при прямом вызове, но не при импорте)
 if __name__== '__main__':
     main()
-
-
